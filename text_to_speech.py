4bd58c9b0f45d19b8f9e938cc622ac367964cb6b
srijena <100935944+srijena@users.noreply.github.com>
2023-01-14 11:08:32 +0530

    Update text_to_speech.py

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,7 +1,7 @@
 import pyttsx3 as tts
 
 engine = tts.init()
-engine.say("A car is coing from left")
+engine.say("A car is coming from left")
 engine.runAndWait()
 
 "https://pypi.org/project/pyttsx3/"
